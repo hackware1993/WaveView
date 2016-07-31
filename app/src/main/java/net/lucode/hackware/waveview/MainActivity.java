d5b908481adc1ce2d2ec1a14cd43c6f30682ffa8
hackware1993 <cfb1993@163.com>
2016-07-31 11:04:40 +0800

    Initial commit

---
+++ app/src/main/java/net/lucode/hackware/waveview/MainActivity.java
@@ -0,0 +1,39 @@
+package net.lucode.hackware.waveview;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateInterpolator;
+
+public class MainActivity extends AppCompatActivity {
+    private WaveView mWaveView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWaveView = (WaveView) findViewById(R.id.wave_view);
+
+        mWaveView.setDuration(5000);
+        mWaveView.setStyle(Paint.Style.STROKE);
+        mWaveView.setSpeed(400);
+        mWaveView.setColor(Color.RED);
+        mWaveView.setInterpolator(new AccelerateInterpolator(1.2f));
+        mWaveView.start();
+
+/*        mWaveView.setDuration(5000);
+        mWaveView.setStyle(Paint.Style.FILL);
+        mWaveView.setColor(Color.RED);
+        mWaveView.setInterpolator(new LinearOutSlowInInterpolator());
+        mWaveView.start();*/
+
+        mWaveView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mWaveView.stop();
+            }
+        }, 10000);
+    }
+}
---
+++ app/src/main/java/net/lucode/hackware/waveview/WaveView.java
@@ -0,0 +1,169 @@
+package net.lucode.hackware.waveview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 水波纹特效
+ * Created by fbchen2 on 2016/5/25.
+ */
+public class WaveView extends View {
+    private float mInitialRadius;   // 初始波纹半径
+    private float mMaxRadiusRate = 0.85f;
+    private float mMaxRadius;   // 最大波纹半径
+    private long mDuration = 2000; // 一个波纹从创建到消失的持续时间
+    private int mSpeed = 500;   // 波纹的创建速度，每500ms创建一个
+    private Interpolator mInterpolator = new LinearInterpolator();
+
+    private List<Circle> mCircleList = new ArrayList<Circle>();
+    private boolean mIsRunning;
+
+    private boolean mMaxRadiusSet;
+
+    private Paint mPaint;
+    private long mLastCreateTime;
+
+    private Runnable mCreateCircle = new Runnable() {
+        @Override
+        public void run() {
+            if (mIsRunning) {
+                newCircle();
+                postDelayed(mCreateCircle, mSpeed);
+            }
+        }
+    };
+
+    public WaveView(Context context) {
+        this(context, null);
+    }
+
+    public WaveView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        setStyle(Paint.Style.FILL);
+    }
+
+    public void setStyle(Paint.Style style) {
+        mPaint.setStyle(style);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (!mMaxRadiusSet) {
+            mMaxRadius = Math.min(w, h) * mMaxRadiusRate / 2.0f;
+        }
+    }
+
+    public void setMaxRadiusRate(float maxRadiusRate) {
+        mMaxRadiusRate = maxRadiusRate;
+    }
+
+    public void setColor(int color) {
+        mPaint.setColor(color);
+    }
+
+    /**
+     * 开始
+     */
+    public void start() {
+        if (!mIsRunning) {
+            mIsRunning = true;
+            mCreateCircle.run();
+        }
+    }
+
+    /**
+     * 缓慢停止
+     */
+    public void stop() {
+        mIsRunning = false;
+    }
+
+    /**
+     * 立即停止
+     */
+    public void stopImmediately() {
+        mIsRunning = false;
+        mCircleList.clear();
+        invalidate();
+    }
+
+    protected void onDraw(Canvas canvas) {
+        Iterator<Circle> iterator = mCircleList.iterator();
+        while (iterator.hasNext()) {
+            Circle circle = iterator.next();
+            float radius = circle.getCurrentRadius();
+            if (System.currentTimeMillis() - circle.mCreateTime < mDuration) {
+                mPaint.setAlpha(circle.getAlpha());
+                canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mPaint);
+            } else {
+                iterator.remove();
+            }
+        }
+        if (mCircleList.size() > 0) {
+            postInvalidateDelayed(10);
+        }
+    }
+
+    public void setInitialRadius(float radius) {
+        mInitialRadius = radius;
+    }
+
+    public void setDuration(long duration) {
+        mDuration = duration;
+    }
+
+    public void setMaxRadius(float maxRadius) {
+        mMaxRadius = maxRadius;
+        mMaxRadiusSet = true;
+    }
+
+    public void setSpeed(int speed) {
+        mSpeed = speed;
+    }
+
+    private void newCircle() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - mLastCreateTime < mSpeed) {
+            return;
+        }
+        Circle circle = new Circle();
+        mCircleList.add(circle);
+        invalidate();
+        mLastCreateTime = currentTime;
+    }
+
+    private class Circle {
+        private long mCreateTime;
+
+        Circle() {
+            mCreateTime = System.currentTimeMillis();
+        }
+
+        int getAlpha() {
+            float percent = (getCurrentRadius() - mInitialRadius) / (mMaxRadius - mInitialRadius);
+            return (int) (255 - mInterpolator.getInterpolation(percent) * 255);
+        }
+
+        float getCurrentRadius() {
+            float percent = (System.currentTimeMillis() - mCreateTime) * 1.0f / mDuration;
+            return mInitialRadius + mInterpolator.getInterpolation(percent) * (mMaxRadius - mInitialRadius);
+        }
+    }
+
+    public void setInterpolator(Interpolator interpolator) {
+        mInterpolator = interpolator;
+        if (mInterpolator == null) {
+            mInterpolator = new LinearInterpolator();
+        }
+    }
+}
